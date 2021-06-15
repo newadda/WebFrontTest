1f8ff9804c7406ba8fce37e1db9feb307c7bdd47
Shin HyunHo <newadda@live.com>
2021-06-15 20:05:28 +0900

    VueJS 테스트 코드

---
+++ vuejs_nodejs/client/src/components/ListTest.vue
@@ -0,0 +1,48 @@
+<template>
+  <div>
+    <button @click="refresh">새로고침</button>
+    <table>
+      <th v-for="(item, index) in columns" :key="index">{{ item }}</th>
+      <tr></tr>
+    </table>
+
+    <p>{{ text }}</p>
+    <button></button>
+  </div>
+</template>
+
+<script>
+const axios = require('axios');
+
+export default {
+  name: 'ListTest',
+  props: {
+    id: String,
+    password: String,
+  },
+  data() {
+    return {
+      columns: ['컬럼1', '컬럼2'],
+      text: '이것',
+      datas: [],
+    };
+  },
+
+  methods: {
+    refresh() {
+      axios
+        .create({
+          url: '',
+          baseURL: 'http://192.168.1.19:8080//api/v1/sms_admin/sms_addrs',
+          method: 'get',
+          // withCredentials: true,
+        })
+        .request()
+        .then((res) => {
+          this.columns = ['zz'];
+          console.log(res);
+        });
+    },
+  },
+};
+</script>
---
+++ vuejs_nodejs/client/src/views/Test.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png">
+    <ListTest></ListTest>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import ListTest from '@/components/ListTest.vue';
+
+export default {
+  name: 'Test',
+  components: {
+    ListTest,
+  },
+};
+</script>
